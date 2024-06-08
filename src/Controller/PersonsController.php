1be48825b03dd31a69f230bc4c207966c6c19687
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2024-06-08 18:49:33 +0000

    refc: リファクタリング

--- src/Controller/PersonsController.php
+++ src/Controller/PersonsController.php
@@ -31,7 +31,7 @@ class PersonsController extends AppController
      * @return \Cake\Http\Response|null|void Renders view
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
+    public function view(?string $id)
     {
         $person = $this->Persons->get($id, contain: ['Genders', 'ServiceLinks']);
         $this->set(compact('person'));
@@ -65,7 +65,7 @@ class PersonsController extends AppController
      * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit(?string $id)
     {
         $person = $this->Persons->get($id, contain: []);
         if ($this->request->is(['patch', 'post', 'put'])) {
@@ -88,7 +88,7 @@ class PersonsController extends AppController
      * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function delete($id = null)
+    public function delete(?string $id)
     {
         $this->request->allowMethod(['post', 'delete']);
         $person = $this->Persons->get($id);
